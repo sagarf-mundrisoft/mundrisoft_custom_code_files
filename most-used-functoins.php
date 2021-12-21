23b23165d5f6602dd3dcb2733bbb05fbdc1e4ff1
Sagar Fegade <84963687+sagarf-mundrisoft@users.noreply.github.com>
2021-12-21 11:58:13 +0530

    Update most-used-functoins.php

--- most-used-functoins.php
+++ most-used-functoins.php
@@ -9,3 +9,20 @@ function show_current_year( $atts ){
   return date('Y');
 }
 add_shortcode( 'current-year', 'show_current_year' );
+
+
+/* Remove the admin bar from the front end */
+add_filter( 'show_admin_bar', '__return_false' );
+
+/* Remove the admin bar from the front end */
+function remove_screen_options(){      
+return false;  
+}  
+add_filter('screen_options_show_screen', 'remove_screen_options');
+
+/* Redirect commenter to thank you post or page */
+add_filter('comment_post_redirect', 'redirect_after_comment');  
+function redirect_after_comment(){       
+wp_redirect('/thank-you-page/');        
+exit();  
+}
